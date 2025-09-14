e081ba755fb54204259802b61afdbdd9524fddfb
Mohammed3MG <mohammedsorguli@.com>
2025-09-14 21:45:33 +0200

    feat(frontend): add new app at frontend/Ainbox and remove old frontend scaffold
    chore: stage current changes and push dev branch

---
+++ frontend/Ainbox/src/App.jsx
@@ -0,0 +1,17 @@
+import ThemeChange from "./components/ThemeChange"
+
+function App() {
+
+
+  return (
+    <>
+      {/* <ThemeChange> </ThemeChange> */}
+
+      <h1 className="text-xl font-bold">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
